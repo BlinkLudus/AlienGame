2ccd4006f2d90e29297a08f07591780067e6d099
CONOR SHEPPARD <conor.sheppard@hotmail.com>
2023-02-20 15:55:29 +0000

    Adding new TestScript

---
+++ Alien Arch - Unity/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public string startMessage;
+    public string anotherMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning(startMessage);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError(anotherMessage);
+    }
+}
