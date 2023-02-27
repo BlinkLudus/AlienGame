765e3c08ac2d63882a6c2ed15a604f5535012ff6
CONOR SHEPPARD <conor.sheppard@hotmail.com>
2023-02-27 14:59:14 +0000

    Adding an Array

--- Alien Arch - Unity/Assets/Scripts/TestScript.cs
+++ Alien Arch - Unity/Assets/Scripts/TestScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    public string[] messages;
     public string message1;
     public string message2;
     public string message3;
@@ -12,22 +13,35 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (messageChoice == 1)
+        for (int i = 0; i < messages.Length; ++i)
         {
-            Debug.Log(message1);
+            Debug.Log(messages[i]);
         }
-        else if (messageChoice == 2)
+        /*if (messageChoice <= messages.Length && messageChoice >= 0)
         {
-            Debug.Log(message2);
-        }
-        else if (messageChoice == 3)
-        {
-            Debug.Log(message3);
+            Debug.Log(messages[messageChoice]);
         }
         else
         {
             Debug.Log("Invalid message choice");
-        }   
+        }*/
+
+        /*if (messageChoice == 1)
+        {
+            Debug.Log(messages[0]);
+        }
+        else if (messageChoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (messageChoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        else
+        {
+            Debug.Log("Invalid message choice");
+        } */
     }
 
     // Update is called once per frame
