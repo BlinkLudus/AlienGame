a49fecfaf7c6782a23fbbe8d0ed9668446502f1c
CONOR SHEPPARD <conor.sheppard@hotmail.com>
2023-03-27 15:01:03 +0100

    Updated jump script

--- Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
+++ Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-
     //Speed settings in metres/second
     public float speed = 1;
+    public float jumpSpeed = 1;
+    public Collider2D groundSensor = null;
+    public LayerMask groundLayer = 0;
 
     //Reference variable so we can use the Rigidbody
     private Rigidbody2D physicsBody = null;
@@ -42,5 +44,22 @@ public class PlayerMovement : MonoBehaviour
 
         //Update our physics component's velocity to be our newly changed value
         physicsBody.velocity = newVelocity;
+    }
+
+    public void Jump()
+    {
+        if (groundSensor.IsTouchingLayers(groundLayer))
+        {
+            //Make a variable to hold our velocity and get 
+            //the current velocity from the physics component
+            Vector2 newVelocity = physicsBody.velocity;
+
+            //Set our velocity to move in the y direction
+            newVelocity.y = jumpSpeed;
+
+            //Update our physics component's velocity to be our newly changed value
+            physicsBody.velocity = newVelocity;
+        }
+      
     }
 }
