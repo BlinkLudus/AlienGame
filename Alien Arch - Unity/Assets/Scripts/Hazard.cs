4ee3ab2cc38abb18ee1f3e44da15f7a60f21da9e
CONOR SHEPPARD <conor.sheppard@hotmail.com>
2023-04-24 14:38:04 +0100

    Added in player health and a hazard

---
+++ Alien Arch - Unity/Assets/Scripts/Hazard.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    public string targetScene = "";
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Get the collider of the object we collided with
+        Collider2D collider = collision.collider;
+
+        // Try to get the player health script attached to that object
+        PlayerHealth player = collider.GetComponent<PlayerHealth>();
+
+        // Check if we found a player health script on the collided object
+        if (player != null)
+        {
+            // Call the Kill function
+            changeScene();
+        }
+       
+        
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+}
