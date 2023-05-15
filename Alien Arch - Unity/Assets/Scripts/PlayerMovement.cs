e1e99bec10c32bb97db763916ec7f87420de1396
BlinkLudus <conor.sheppard@hotmail.com>
2023-05-15 10:52:31 +0100

    Updated animation

--- Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
+++ Alien Arch - Unity/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,24 @@ public class PlayerMovement : MonoBehaviour
     //Reference variable so we can use the Rigidbody
     private Rigidbody2D physicsBody = null;
 
+    void Update()
+    {
+        Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D>();
+
+        //Find out from the rigidbody what our current horizontal and vertical speeds are
+        float currentSpeedH = ourRigidBody.velocity.x;
+        float currentSpeedV = ourRigidBody.velocity.y;
+
+        //Get the animator component that will be setting our animation
+        Animator ourAnimator = GetComponent<Animator>();
+
+        //Tell our animator what the speeds are
+        ourAnimator.SetFloat("speedH", currentSpeedH);
+        ourAnimator.SetFloat("speedV", currentSpeedV);
+    }
+
+
+
     private void Awake()
     {
         physicsBody = GetComponent<Rigidbody2D>();
